4d629b4c1a12977e09d7e2b5dd514d80a3155e6f
greanleeph <82634258+greanleeph@users.noreply.github.com>
2025-05-03 16:22:20 +0800

    Delete compiler/windows directory

--- compiler/windows/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Assemble the Parrot assembly to object code
-nasm -f win64 parrot_assembly.asm -o parrot_output.obj
-if errorlevel 1 exit /b
-
-REM Link the object file into an executable
-link /subsystem:console parrot_output.obj /defaultlib:kernel32.lib /defaultlib:msvcrt.lib /entry:main /out:parrot.exe
